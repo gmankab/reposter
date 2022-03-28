6481b8cff6def0facbb371627c4d52393b41c69d
gmankab <gmankab@gmail.com>
2022-03-28 19:22:19 +0300

    aboba

--- backupper.py
+++ backupper.py
@@ -3,43 +3,97 @@ script to backup
 '''
 
 # importing libraries
-from audioop import reverse
-from operator import ge
+from urllib import request as r
+import subprocess
 import pathlib
-import asyncio
-import logging
 import shutil
 import sys
 import os
 
 
-# logging.disable(logging.CRITICAL)
-# disabling annoying warnings messages
+def clean_path(path):
+    path = str(path).replace('\\', '/')
+    # conerting a\\b///\\\c\\/d/e/ to a//b//////c///d/e/
+
+    # conerting a//b//////c///d/e/ to a/b/c/d/e/
+    while '//' in path:
+        path = path.replace('//', '/')
+    return path
+
+
+def get_parrent_dir(file):
+    return clean_path(
+        pathlib.Path(file).parent
+    )
 
 
 # getting Current Work Dir path
-cwd = str(
-    pathlib.Path(__file__).parent
-).replace('\\', '/')
-
-
-while '//' in cwd:
-    cwd = cwd.replace('//', '/')
-# conerting a\\b\\\\\\\c///d\e/ to a/b/c/d/e/
-
+cwd = get_parrent_dir(__file__)
 
 # adding libs and files from Current Work Dir to sys.path
 # this is needed so that python can find this libs and files, then use them
 sys.path += (
     cwd,
     f'{cwd}/libs',
-    f'{cwd}/dev_libs.zip',
 )
 
+downloads = f'{cwd}/downloads'
 
+
+def install_libs():
+    print('checking libs')
+    pip = f'{sys.executable} -m pip'
+
+    requirements = [
+        'rich'
+        'pyrogram'
+        'tgcrypto'
+        'pyyaml'
+    ]
+
+    try:
+        for requirement in requirements:
+            __import__(requirement)
+    except ImportError:
+        # clearing pip chache
+        output = subprocess.getstatusoutput(
+            f'{pip} cache purge'
+        )[1]
+        print(output)
+        if 'No module named pip' in output:
+            print('installing pip...')
+            # pip is a shit which allow to install libs, so if we want to install libs we must have pip
+            py_dir = get_parrent_dir(sys.executable)
+
+            # fixing shit which doesn't allow to install pip in python embedable in windows:
+            for file in os.listdir(
+                py_dir
+            ):
+                if file[-5:] == '._pth':
+                    with open(f'{py_dir}/file', 'r+') as file:
+                        print(file.read())
+
+            # instaling pip:
+            get_pip = f'{downloads}/get-pip.py'
+            if not os.path.isdir(downloads):
+                os.mkdir(downloads)
+
+            r.urlretrieve(
+                url = 'https://bootstrap.pypa.io/get-pip.py',
+                filename = f'{downloads}/get-pip.py',
+            )
+
+        # os.popen(f'{pip} install -U pip {" ".join(requirements)}')
+
+
+install_libs()
+
+
+import pyrogram
+
+# this lib needed for beautiful output:
 from rich import print
 from rich import pretty
-import pyrogram
 pretty.install()
 
 
@@ -59,115 +113,131 @@ app = pyrogram.Client(
 )
 
 
+def clear_dir(dir):
+    shutil.rmtree(
+        dir,
+        ignore_errors=True,
+    )
+
+
 def show_progress(current, total):
     print(f'{current}/{total}')
 
 
-def main():
-    with app:
-        target = 'me'
-        downloads = f'{cwd}/downloads/'
+def save(
+    msg,
+):
+    '''
+    save file and return path to it
+    '''
+    clear_dir(downloads)
 
-        # removing chache dir
-        shutil.rmtree(
-            downloads,
-            ignore_errors=True,
+    if not os.path.isdir(downloads):
+        os.mkdir(downloads)
+    app.download_media(
+        msg,
+        file_name = downloads,  # path to save file
+        progress = show_progress,
+    )
+    downloaded = os.listdir(downloads)
+    if downloaded:
+        return f'{downloads}/{downloaded[0]}'  # returns saved file path
+    else:
+        print(
+            'nothing downloaded from this message:',
+            msg,
+            'please contact developer',
+            sep = '\n'
         )
 
-        def save(msg):  # save file and return path to it
-            if not os.path.isdir(downloads):
-                os.mkdir(downloads)
-            app.download_media(
-                msg,
-                file_name = downloads,  # path to save file
+
+def backup(
+    msg,
+    target = 'me',
+):
+    print(msg)
+    if msg.service:
+        return
+    if msg.text:
+        app.send_message(
+            target,
+            msg.text,
+        )
+
+    if msg.caption:
+        app.send_message(
+            target,
+            msg.caption,
+        )
+
+    if msg.document:
+        print('saving document')
+        path = save(msg)
+        print(f'uploading "{path}"')
+        app.send_document(
+            target,
+            document = path,
+            progress = show_progress,
+        )
+        os.remove(path)
+    if msg.media:
+        if msg.photo:
+            print('saving photo')
+            path = save(msg)
+            print(f'uploading {path}')
+            app.send_photo(
+                target,
+                photo=path,
                 progress = show_progress,
             )
-            downloaded = os.listdir(downloads)
-            if downloaded:
-                return downloads + downloaded[0]  # returns saved file path
-            else:
-                raise FileExistsError(
-                    f'nothing downloaded from this message:\n{msg}'
-                )
+            os.remove(path)
 
-        def backup(msg):
-            print(msg)
-            if msg.service:
-                return
-            if msg.text:
-                app.send_message(
-                    target,
-                    msg.text,
-                )
+        if msg.voice:
+            print('saving voice')
+            path = save(msg)
+            print(f'uploading {path}')
+            app.send_voice(
+                target,
+                path,
+                progress = show_progress,
+            )
+            os.remove(path)
 
-            if msg.caption:
-                app.send_message(
-                    target,
-                    msg.caption,
-                )
+        if msg.audio:
+            print('saving audio')
+            path = save(msg)
+            print(f'uploading {path}')
+            app.send_document(
+                target,
+                path,
+                progress = show_progress,
+            )
+            os.remove(path)
 
-            if msg.document:
-                print('saving document')
-                path = save(msg)
-                print(f'uploading {path}')
-                app.send_document(
-                    target,
-                    document = path,
-                    progress = show_progress,
-                )
-                os.remove(path)
+        if msg.video:
+            print('saving video')
+            path = save(msg)
+            print(f'uploading {path}')
+            app.send_video(
+                target,
+                path,
+                progress = show_progress,
+            )
+            os.remove(path)
 
-            if msg.media:
-                if msg.photo:
-                    print('saving photo')
-                    path = save(msg)
-                    print(f'uploading {path}')
-                    app.send_photo(
-                        target,
-                        photo=path,
-                        progress = show_progress,
-                    )
-                    os.remove(path)
 
-                if msg.voice:
-                    print('saving voice')
-                    path = save(msg)
-                    print(f'uploading {path}')
-                    app.send_voice(
-                        target,
-                        path,
-                        progress = show_progress,
-                    )
-                    os.remove(path)
-
-                if msg.audio:
-                    print('saving audio')
-                    path = save(msg)
-                    print(f'uploading {path}')
-                    app.send_document(
-                        target,
-                        path,
-                        progress = show_progress,
-                    )
-                    os.remove(path)
-
-                if msg.video:
-                    print('saving video')
-                    path = save(msg)
-                    print(f'uploading {path}')
-                    app.send_video(
-                        target,
-                        path,
-                        progress = show_progress,
-                    )
-                    os.remove(path)
-
+def main():
+    with app:
         print('getting messages')
         for msg in app.iter_history(
-            -1001655504016,
-            reverse = True
+            chat_id = -1001655504016,
+            limit = None,
+            offset_id = 8,
+            reverse = True,
         ):
             backup(msg)
+            # print(msg)
+    clear_dir(downloads)
 
 
 main()
