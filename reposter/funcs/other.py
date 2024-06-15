625856282b4826a252745724acd6e1a5aa82e661
gmankab <gmankab@gmail.com>
2024-06-15 16:51:57 +0400

    [f] colors

--- reposter/funcs/other.py
+++ reposter/funcs/other.py
@@ -61,5 +61,5 @@ def double_links(
     source_msg: pyrogram.types.Message,
     target_msg: pyrogram.types.Message,
 ) -> str:
-    return f'[blue]{single_link(source_msg)}[/] -> [yellow]{single_link(target_msg)}'
+    return f'[blue]{single_link(source_msg)}[/] -> [yellow]{single_link(target_msg)}[/]'
 
